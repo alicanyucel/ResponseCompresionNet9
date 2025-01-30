ebef99f6672e8f0999e9e10cac454810dd71c784
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-30 16:16:27 +0300

    swagger eklendi.

--- ResponseCompresionNet9/Program.cs
+++ ResponseCompresionNet9/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -11,7 +11,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
