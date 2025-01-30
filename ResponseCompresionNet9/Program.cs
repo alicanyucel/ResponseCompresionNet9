5466dc069be0c95939097c2f4ec4f412be1fd702
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-30 16:33:44 +0300

    bitti.

--- ResponseCompresionNet9/Program.cs
+++ ResponseCompresionNet9/Program.cs
@@ -1,8 +1,26 @@
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.ResponseCompression;
+using System.IO.Compression;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSwaggerGen();
+builder.Services.AddResponseCompression(opt =>
+{
+    opt.EnableForHttps = true;
+    opt.Providers.Clear();
+    // güncel browserlada daha iyi sýkýstýrýyor
+   // opt.Providers.Add<BrotliCompressionProvider>();
+    // tüm browserlarda performans zayýf
+    opt.Providers.Add<GzipCompressionProvider>();
+});
+builder.Services.Configure<GzipCompressionProviderOptions>(options =>
+{
+    options.Level = CompressionLevel.Fastest;
+});
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -20,5 +38,14 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseResponseCompression();
+app.MapGet("/GetAll", () =>
+{
+    List<int> numbers = new();
+    for(int i = 0; i <= 100; i++)
+    {
+        numbers.Add(i);
+    }
+    return numbers;
+});
 app.Run();
